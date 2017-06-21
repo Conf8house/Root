7987bcf68fa3271cf793193b8d87252cafe6b6f7
Conf8house <rin_ruger@mail.ru>
2017-06-22 00:41:30 +1000

    Sqrt and parse

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,24 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            Single X;
+            bool Number_pi = Single.TryParse(
+                textBox1.Text,
+                System.Globalization.NumberStyles.Number,
+                System.Globalization.NumberFormatInfo.CurrentInfo,
+                out X);
+            if (Number_pi == false)
+            {
+                //Если пользователь ввел не число:
+                label1.Text = "Следует вводить числа";
+                label1.ForeColor = Color.Red; //цвет текса на метке
+                return; //Выход из процедуры или Return
+            }
+            //Извлечение корня с преобразованием в тип Single:
+            var Y = (Single)Math.Sqrt(X);
+            //или var Y = Convert.ToSingle(Math.Sqrt(X));
+            label1.ForeColor = Color.Black;
+            label1.Text = string.Format(""); 
         }
 
         private void Form1_Load(object sender, EventArgs e)
